d54dbbfee3892615e6ef85499b28847abd93fb10
songqijian <songqijian@daqo.com>
2021-04-22 09:16:11 +0800

    s

---
+++ lib/hello_world.dart
@@ -0,0 +1,5 @@
+library hello_world;
+
+export 'src/page_a.dart';
+export 'src/page_b.dart';
+export 'src/page_c.dart';
---
+++ lib/src/page_b.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PageB extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.pink,
+      child: Text('page_b'),
+    );
+  }
+}
